08132c62da27c9b811d0b56d009276cd1472fd81
JoniKarta <Jkarta26@gmail.com>
2020-12-09 13:10:13 +0200

    added docs

--- src/main/java/com/example/demo/ShoppingProductServiceApplication.java
+++ src/main/java/com/example/demo/ShoppingProductServiceApplication.java
@@ -3,6 +3,9 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
 public class ShoppingProductServiceApplication {
 
